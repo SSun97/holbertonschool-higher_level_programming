5fc4eec4a811aa1a114f2b64f29ceb616a3e8a9c
Simon Sun <sunmingyu.97@gmail.com>
2021-04-26 20:16:51 +0000

    add code for null list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,13 +8,15 @@ int check_cycle(listint_t *list)
 {
 	listint_t *p1 = list, *p2 = list;
 
-	while (p1 && p2)
+	if (list)
 	{
-		p1 = p1->next;
-		p2 = p2->next->next;
-		if (p1 == p2)
-			return (1);
-
+		while (p1 && p2)
+		{
+			p1 = p1->next;
+			p2 = p2->next->next;
+			if (p1 == p2)
+				return (1);
+		}
 	}
 	return (0);
 }
