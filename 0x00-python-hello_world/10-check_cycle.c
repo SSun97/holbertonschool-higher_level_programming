9c8cfd401c4599d21e6c15ee8556f48a8b333894
Simon Sun <sunmingyu.97@gmail.com>
2021-04-26 20:12:46 +0000

    first push

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - check if there is a cycle on the list
+ * @list: type listint_t
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *p1 = list, *p2 = list;
+
+	while (p1 && p2)
+	{
+		p1 = p1->next;
+		p2 = p2->next->next;
+		if (p1 == p2)
+			return (1);
+
+	}
+	return (0);
+}
