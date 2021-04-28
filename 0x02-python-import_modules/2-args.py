957fa16bf18414bbc8864f5576c196509a9c172c
Simon Sun <sunmingyu.97@gmail.com>
2021-04-28 02:07:03 +0000

    sigle and plural argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    print("{} arguments.".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    n = len(sys.argv)
+    if n == 1:
+        print("0 arguments.")
+    else:
+        print("{} argument{}".format(len(sys.argv) - 1,
+              ":" if len(sys.argv) == 2 else "s:"))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
