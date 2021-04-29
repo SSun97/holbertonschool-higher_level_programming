7b00ffe464c8ee7a4299f949866aa06534922557
Simon Sun <sunmingyu.97@gmail.com>
2021-04-29 01:06:24 +0000

    first push

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - check if a linked list is palindrome
+ * @head: double pointer of listint_t
+ * Return: 1 if it is, otherwise 0
+ */
+int is_palindrome(listint_t **head)
+{
+	int values[10000];
+	size_t a = 0, i, j;
+
+	while (*head != NULL)
+	{
+		values[a++] = (*head)->n;
+		*head = (*head)->next;
+	}
+	for (i = 0, j = a - 1; i < j; ++i, --j)
+	{
+		if (values[i] != values[j])
+			return (0);
+	}
+
+	return (1);
+}
