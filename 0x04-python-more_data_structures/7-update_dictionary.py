31c6d1835f17fab4ab5312efce04aa4c423c8436
Simon Sun <sunmingyu.97@gmail.com>
2021-04-29 22:11:10 +0000

    first push

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
+
+#   for k in a_dictionary.keys():
+#     if k == key:
+#       a_dictionary[k] = value
+#       return a_dictionary
+#     else:
+#       a_dictionary[k] = value
+#       return a_dictionary
