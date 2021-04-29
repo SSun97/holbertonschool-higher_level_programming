a0f70914f9ad09ba8c5d2606c287b039dabe4ac6
Simon Sun <sunmingyu.97@gmail.com>
2021-04-29 19:14:08 +0000

    short version

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 and set_2:
-        return {item for item in set_1 if item in set_2}
-    else:
-        return {}
+    return set_1 & set_2
+#    if set_1 and set_2:
+#        return {item for item in set_1 if item in set_2}
+#    else:
+#        return {}
