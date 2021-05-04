26858a3c9dea4b09f2d24ec1855df24896323f16
Simon Sun <sunmingyu.97@gmail.com>
2021-05-04 14:17:18 +0000

    first push

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A class Square that defines a square
+
+This module provides a simple version of class
+
+"""
+
+
+class Square:
+    """The class definition"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
