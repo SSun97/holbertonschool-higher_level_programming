4badc88026cd5da189bff99c3cdec917d6c14554
Simon Sun <sunmingyu.97@gmail.com>
2021-05-04 13:41:45 +0000

    first push

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A empty class Square that defines a square
+
+This module provides a simple version of class
+
+"""
+
+
+class Square:
+    """The class definition"""
+
+    pass
