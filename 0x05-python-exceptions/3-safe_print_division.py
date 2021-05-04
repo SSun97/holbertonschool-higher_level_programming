cd1b473e5a8d7f442ff58ef5abc43f9852a6084d
Simon Sun <sunmingyu.97@gmail.com>
2021-05-04 12:33:17 +0000

    use formate to print the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,5 +9,5 @@ def safe_print_division(a, b):
             print("Inside result: {}".format(div))
             return div
         else:
-            print("Inside result: None")
+            print("Inside result: {}".format(None))
             return None
