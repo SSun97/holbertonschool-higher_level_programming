0a7943e45eb856967201619bf268977db7f49f21
Simon Sun <sunmingyu.97@gmail.com>
2021-05-04 16:01:07 +0000

    rename the variable

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,14 +19,14 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, v):
         """set the size"""
-        if type(size) is not int:
+        if type(v) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif v < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = v
 
     def area(self):
         """calculate the area"""
