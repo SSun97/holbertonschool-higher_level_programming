523e3d47014b00526d75fba1631828a43b782238
Simon Sun <sunmingyu.97@gmail.com>
2021-05-04 14:24:38 +0000

    first commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A empty class Square that defines a square
+
+This module provides a simple version of class
+
+"""
+
+
+class Square:
+    """The class definition"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
