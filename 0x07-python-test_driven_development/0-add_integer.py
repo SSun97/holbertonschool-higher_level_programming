6a6a8ae6f582006d383440ffcf34b4a8909137c5
Simon Sun <sunmingyu.97@gmail.com>
2021-05-05 17:46:29 +0000

    first push

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    A module to add two integers
+
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+        A function to add two integers
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
