2eba78cc5a58dcf8e8090a0a2083076b5ec37bf2
Simon Sun <sunmingyu.97@gmail.com>
2021-05-06 01:08:42 +0000

    first push

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    prints a square with the character #
+
+"""
+
+
+def print_square(size):
+    """
+        a function that prints prints a square with the character #
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
