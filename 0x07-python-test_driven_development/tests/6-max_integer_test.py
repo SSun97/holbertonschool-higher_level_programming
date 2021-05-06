35194513f5e8a16f7c93fbffddd8f678576d1c3c
Simon Sun <sunmingyu.97@gmail.com>
2021-05-06 02:50:25 +0000

    first push

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test max_integer.py"""
+
+    def test_max_integer(self):
+        """Test method max_integer(list=[])"""
+
+        self.assertEqual(4, max_integer([1, 2, 3, 4]))
+        self.assertEqual(4, max_integer([1, 2, 4, 3]))
+
+if __name__ == '__main__':
+    unittest.main()
