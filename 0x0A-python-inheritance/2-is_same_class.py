3a04cf82bdd70f7335ba5876e23a9a0bd053f2dd
Simon Sun <sunmingyu.97@gmail.com>
2021-05-11 20:09:31 +0000

    first push

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Write a function that returns True if the object is exactly an\
+     instance of the specified class ; otherwise False
+"""
+
+
+def is_same_class(obj, a_class):
+    """Doc """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
