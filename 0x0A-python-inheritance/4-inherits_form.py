3f3633a1995ea2e3baec916c431e8f8cd651edb2
Simon Sun <sunmingyu.97@gmail.com>
2021-05-11 20:25:05 +0000

    first push

---
+++ 0x0A-python-inheritance/4-inherits_form.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Write a function that returns True if the object is an instance\
+     of a class that inherited (directly or indirectly) from the\
+      specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Doc"""
+
+    if type(obj) == a_class:
+        return False
+    elif issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
