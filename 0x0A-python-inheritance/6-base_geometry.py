f28feec422fb34f8ae16b0b5621155b47183e355
Simon Sun <sunmingyu.97@gmail.com>
2021-05-11 20:33:13 +0000

    first push

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Write an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Write an empty class BaseGeometry"""
+
+    def area(self):
+        """area function"""
+
+        raise Exception("area() is not implemented")
