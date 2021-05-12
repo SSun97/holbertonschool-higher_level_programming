3158419411f3ed29e7a0387da311e24f7446f889
Simon Sun <sunmingyu.97@gmail.com>
2021-05-12 19:14:48 +0000

    first push

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ a function to read a file"""
 
-    with open('my_file_0.txt', 'r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
