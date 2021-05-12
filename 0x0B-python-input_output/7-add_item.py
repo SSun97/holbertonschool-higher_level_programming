59c2cc0a27b0602ab37ca81a40e099d999b03dc1
Simon Sun <sunmingyu.97@gmail.com>
2021-05-12 21:46:46 +0000

    first push

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A script to add everything """
+
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+items = sys.argv
+items.pop(0)
+
+try:
+    with open("add_item.json", 'x') as f:
+        save_to_json_file(items, "add_item.json")
+except:
+    newlist = load_from_json_file("add_item.json")
+    save_to_json_file(newlist + items, "add_item.json")
