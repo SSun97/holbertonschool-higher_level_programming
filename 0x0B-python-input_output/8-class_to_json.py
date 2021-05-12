4d48a4b23ada179adc510bfc196b6700d6c28784
Simon Sun <sunmingyu.97@gmail.com>
2021-05-12 22:03:32 +0000

    first push

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ a module returns the dictionary description with simple data
      structure (list, dictionary, string, integer and boolean)
      for JSON serialization of an object:
