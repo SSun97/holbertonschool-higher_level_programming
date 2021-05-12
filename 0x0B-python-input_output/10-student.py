a2d6937bf62d2f54e7b22df064f426f4684056d0
Simon Sun <sunmingyu.97@gmail.com>
2021-05-12 22:35:36 +0000

    first push

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,7 +10,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         if type(attrs) is list:
             return {k: v for k, v in self.__dict__.items() if k in attrs}
         else:
