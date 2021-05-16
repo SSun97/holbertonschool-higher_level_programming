0cd17e8049d692fdf1ccd2737d62466c5e6cb149
Simon Sun <sunmingyu.97@gmail.com>
2021-05-16 16:16:38 +0000

    first push

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,6 +24,15 @@ class Rectangle(Base):
         for _ in range(self.__height):
             print("#" * self.__width)
 
+        def __str__(self):
+            """print function"""
+
+            return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__,
+                                                    self.id, self.__x,
+                                                    self.__y,
+                                                    self.__width,
+                                                    self.__height)
+
     @property
     def width(self):
         return self.__width
