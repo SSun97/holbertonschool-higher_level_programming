9907bf01fa9632c9ca49993f5222e7b804aa747b
Simon Sun <sunmingyu.97@gmail.com>
2021-05-16 22:07:01 +0000

    first push

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
+        self.size = size
 
     def __str__(self):
         return "[{}] ({}) {}/{} - {}".format(type(self).__name__,
