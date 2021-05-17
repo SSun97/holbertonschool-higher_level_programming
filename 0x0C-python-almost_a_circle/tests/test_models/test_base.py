aa6a3a981d19c64adc84dd7b35e9f5790d892f87
Simon Sun <sunmingyu.97@gmail.com>
2021-05-17 19:51:11 +0000

    first push

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,7 +4,8 @@
 
 import unittest
 from models.base import Base
-
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestClassMerthods(unittest.TestCase):
     """test the almost a circle project"""
@@ -14,6 +15,12 @@ class TestClassMerthods(unittest.TestCase):
 
     def test_Base_id_not_empty(self):
         self.assertEqual(Base(89).id, 89)
+
+    def test_Base_to_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(json_dictionary), str)
 
 
 
