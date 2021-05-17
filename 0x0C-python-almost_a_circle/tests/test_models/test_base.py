78674feb25443d096dd925d81c928a2d122a3e0f
Simon Sun <sunmingyu.97@gmail.com>
2021-05-17 16:29:04 +0000

    first push

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,16 +11,12 @@ class TestClassMerthods(unittest.TestCase):
 
     def test_Base_id(self):
         self.assertEqual(Base().id, 1)
+
+    def test_Base_id_not_empty(self):
         self.assertEqual(Base(89).id, 89)
 
 
-    def test_A_nb_objects_private(self):
-        '''Tests if nb_objects is private class attribute.'''
-        self.assertTrue(hasattr(Base, "_Base__nb_objects"))
 
-    def test_B_nb_objects_initialized(self):
-        '''Tests if nb_objects initializes to zero.'''
-        self.assertEqual(getattr(Base, "_Base__nb_objects"), 0)
 
 
 if __name__ == '__main__':
