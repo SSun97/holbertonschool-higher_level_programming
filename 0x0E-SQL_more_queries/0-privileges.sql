85516f90a9a58c82d49c0c88d2f287e47e65fcf4
Simon Sun <sunmingyu.97@gmail.com>
2021-05-27 17:14:25 +0000

    first push

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,4 @@
 -- list all privileges of MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- list privileges of user2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
