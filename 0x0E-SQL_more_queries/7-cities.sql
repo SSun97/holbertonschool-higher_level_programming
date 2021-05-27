4edb690baa90156bbe46e5b9d55b9473e067b2d1
Simon Sun <sunmingyu.97@gmail.com>
2021-05-27 20:29:00 +0000

    first push

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	state_id INT NOT NULL, 
 	name VARCHAR(256) NOT NULL, 
 	PRIMARY KEY (id), 
-	FOREIGN KEY (state_id) REFERENCE hbtn_0d_usa.states (id));
+	FOREIGN KEY (state_id) REFERENCE cities (id));
