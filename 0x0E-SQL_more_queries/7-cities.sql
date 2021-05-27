b6519e9d8ddf5776e04c4121661dcd0de15e98da
Simon Sun <sunmingyu.97@gmail.com>
2021-05-27 20:15:40 +0000

    first push

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,3 @@
+-- create a database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
