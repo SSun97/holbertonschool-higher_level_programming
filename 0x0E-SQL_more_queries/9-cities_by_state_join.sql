82d1398df5205a7929a90d07cac5a7f8e5e7df15
Simon Sun <sunmingyu.97@gmail.com>
2021-05-27 21:34:55 +0000

    first push

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --  lists all cities contained in the database
 SELECT cities.id AS id, cities.name AS name, states.name AS name
 FROM cities LEFT JOIN states
-ON states.id = cities.states_id
+ON states.id = cities.state_id
 ORDER BY cities.id;
