a88b6165a17dca907d51e08b7efdb33ce9709813
Simon Sun <sunmingyu.97@gmail.com>
2021-05-27 20:50:53 +0000

    first push

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- lists all the cities of California that can be found in the database
+SELECT id, name 
+FROM hbtn_0d_usa.cities 
+WHERE state_id = 
+	(SELECT id 
+	 FROM hbtn_0d_usa.states
+	 WHERE name = 'California');
