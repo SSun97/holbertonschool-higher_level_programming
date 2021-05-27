0c45607ebf4eb32ab044a64a80639c4f6e623c20
Simon Sun <sunmingyu.97@gmail.com>
2021-05-27 21:26:05 +0000

    first push

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 --  lists all cities contained in the database
 SELECT cities.id AS id, cities.name AS name, states.name AS name
-FROM cities NATURAL JOIN states;
+FROM cities NATURAL JOIN states
+WHERE states.id = cities.states_id;
