7cf81a207a3df6f3bc982270a8ddb6f4be1c3bf7
Simon Sun <sunmingyu.97@gmail.com>
2021-05-31 16:31:14 +0000

    first push

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Select a table in a database"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+         host="localhost",
+         user=sys.argv[1],
+         passwd=sys.argv[2],
+         db=sys.argv[3],
+         port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+
+    for row in cur.fetchall():
+        if row[1][0] == 'N':
+            print(row)
+
+    db.close()
