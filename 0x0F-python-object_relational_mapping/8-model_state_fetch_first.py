d46abdc1836ed1e14dbe594b6c8834608cb979dc
Simon Sun <sunmingyu.97@gmail.com>
2021-05-31 22:47:13 +0000

    first push

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+    state = session.query(State).order_by(State.id).first()
+    if state:
         print("{}: {}".format(state.id, state.name))
-        break
+    else:
+        print("Nothing")
     session.close()
