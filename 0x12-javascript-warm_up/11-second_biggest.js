5862283a30d976c72d737bd843133e6caf93d4a4
Simon Sun <sunmingyu.97@gmail.com>
2021-06-03 03:33:37 +0000

    first push

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length < 4) {
-  console.log(0);
-} else {
-  let first = process.argv[2];
-  let second = process.argv[3];
-  if (first < second) {
-    [first, second] = [process.argv[3], process.argv[2]];
-  }
-  for (let i = 4; i < process.argv.length; i++) {
-    if (process.argv[i] >= first) {
-      second = first;
-      first = process.argv[i];
-    } else if (process.argv[i] <= first && process.argv[i] >= second) {
-      second = process.argv[i];
-    }
-  }
-
-  console.log(second);
-}
+console.log(process.argv.length < 4 ? 0 : process.argv.slice(2).sort((a, b) => b - a)[1]);
