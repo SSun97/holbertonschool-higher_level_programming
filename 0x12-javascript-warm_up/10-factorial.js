1fabe9efadd8b0872582abf297d0018a30082403
Simon Sun <sunmingyu.97@gmail.com>
2021-06-03 02:35:08 +0000

    first push

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function factorial (a) {
-  if (a == NaN || a === 1) {
+  if (isNaN(a) || a === 1) {
     return 1;
   } else {
-      return a * factorial( a - 1);
-    }
+    return a * factorial(a - 1);
+  }
 }
-n = parseInt(process.argv[2]);
+const n = parseInt(process.argv[2]);
 console.log(factorial(n));
