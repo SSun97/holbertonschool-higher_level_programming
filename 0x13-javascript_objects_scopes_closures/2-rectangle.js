8006c3797425c27354e37c02f3e5a5a8f684beef
Simon Sun <sunmingyu.97@gmail.com>
2021-06-04 02:04:35 +0000

    first push

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Rectangle Class with restrictions
+class Rectangle {
+  constructor (w, h) {
+    w = parseInt(w);
+    h = parseInt(h);
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
